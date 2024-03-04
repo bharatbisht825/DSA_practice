d8345d8a6d0ba55d856ab6b8b23b01cad3076cfa
bharatbisht825 <bharatbisht825@gmail.com>
2024-03-04 09:56:18 +0530

    third

--- max.c
+++ max.c
@@ -2,12 +2,14 @@
 #include<stdlib.h>
 void main(){
     int ar[5]={4,5,6,7,8};
-    int sum=0,length=5;
+    int max=ar[0];
     for(int i=0;i<5;i++){
-        sum=sum+ar[i];
-        }
-    printf("%d",(sum/length));
+       if(ar[i]>max){
+        max=ar[i];
+       }
+        
        
-    
+    }
+    printf("%d",max);
 
 }
--- min.c
+++ min.c
@@ -2,12 +2,14 @@
 #include<stdlib.h>
 void main(){
     int ar[5]={4,5,6,7,8};
-    int sum=0,length=5;
+    int min=ar[0];
     for(int i=0;i<5;i++){
-        sum=sum+ar[i];
-        }
-    printf("%d",(sum/length));
+       if(ar[i]<min){
+        min=ar[i];
+       }
+        
        
-    
+    }
+    printf("%d",min);
 
 }
